517ccc3104cbed69e9d59901d6e4410ad488ebbe
Line Reinsaas <line.beathe@gmail.com>
2012-09-29 18:52:12 +0200

    Copying Project

---
+++ Level/src/com/example/level/SensorActivity.java
@@ -0,0 +1,35 @@
+package com.example.level;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class SensorActivity extends Activity implements SensorEventListener {
+    private final SensorManager mSensorManager;
+    private final Sensor mSensor;
+
+    public SensorActivity() {
+    	mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    	mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+    }
+}
