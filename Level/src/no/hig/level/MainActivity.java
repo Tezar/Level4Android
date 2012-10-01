aeb816b936d39fe084229445cceb4361d6e055f3
Ales Tomecek <TomecekAles@seznam.cz>
2012-10-01 12:04:44 +0200

    moved sensing code to MainActivity

--- Level/src/no/hig/level/MainActivity.java
+++ Level/src/no/hig/level/MainActivity.java
@@ -2,18 +2,54 @@ package no.hig.level;
 
 
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    	mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    	mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    	
+    	//todo: test if sensor really exists
+
     }
 
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+    }
+    
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
